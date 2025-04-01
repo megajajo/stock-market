a4a1b42be72aa888d8fd34bb8b064cecccf73834
Stefan Domunco <stefan.domunco@stcatz.ox.ac.uk>
2025-04-01 23:59:25 +0300

    update on the test files (generating + checking), will continue tomorrow. Does not work at the moment.

--- OrderBook/testChecker.py
+++ OrderBook/testChecker.py
@@ -10,22 +10,34 @@ This works as a "debugger".
 """
 
 
-def print_clients(clients, fileName):
+def write_clients(clients, fileName):
     f = open(
         f"OrderBook/TestLog/{fileName}", "a"
     )  # assume it is run from the stock-market folder
+
     f.write("Clients:\n")
     for client in clients:
         f.write(f"ID: {client.get_id()}, Balance: {client.get_balance()}\n")
-    f.write("\n")
+    f.write("\n" + "--" * 30 + "\n")
 
 
-def print_list(orderBook, fileName):
+def write_list(orderBook, fileName, last_order=None):
     asks = orderBook.export_asks()
     bids = orderBook.get_all_bids()
     f = open(
         f"OrderBook/TestLog/{fileName}", "a"
     )  # assume it is run from the stock-market folder
+
+    if last_order is not None:
+        if last_order.side == BuyOrSell.BUY:
+            f.write(
+                f"Last processed order:    BUY: {last_order.client.get_id()}, {last_order.price} @ {last_order.volume}\n"
+            )
+        else:
+            f.write(
+                f"Last processed order:    SELL: {last_order.client.get_id()}, {last_order.price} @ {last_order.volume}\n"
+            )
+
     f.write("Asks:\n")
     for order in asks:
         f.write(f"ID: {order[0]}, Price: {order[2]}, Volume: {order[3]} \n")
@@ -37,31 +49,30 @@ def print_list(orderBook, fileName):
     f.write("\n")
 
 
+# What if there is an error while doing this? TODO
 def run_OrderBook(clients, orders):
     timestamp = datetime.now(timezone.utc)
     fileName = str(timestamp) + ".txt"
     f = open(
         f"OrderBook/TestLog/{fileName}", "a"
     )  # assume it is run from the stock-market folder
-    f.write("Clients:\n")
 
     ob = OrderBook("JPK")
-    print_clients(clients, fileName)
+    write_clients(clients, fileName)
     for order in orders:
         if order.side == BuyOrSell.BUY:
             ob.place_order(BUY, order.price, order.volume, order.client.get_id())
-            f.write(
-                f"Last processed order:    BUY: {order.client.get_id()}, {order.price} @ {order.volume}\n"
-            )
 
         else:
             ob.place_order(SELL, order.price, order.volume, order.client.get_id())
-            f.write(
-                f"Last processed order:    SELL: {order.client.get_id()}, {order.price} @ {order.volume}\n"
-            )
 
-        print_list(ob, fileName)
-        print_clients(clients, fileName)
+        write_list(ob, fileName, order)
+        write_clients(clients, fileName)
+
+
+def checkValidity():
+    # TODO: implement this function to check the validity of the order book and clients
+    pass
 
 
 if __name__ == "__main__":
@@ -93,9 +104,18 @@ if __name__ == "__main__":
         action="store_true",
         help="Generate everything at random, ignoring clients and orders",
     )
+    parser.add_argument(
+        "-nt",
+        "--newtest",
+        action="store_true",
+        help="Generate a new test before simulating the order book",
+    )
     args = parser.parse_args()
 
-    clients, orders = generateTest(
-        args.allrandom, args.clients, args.orders, args.tickers
-    )
+    if args.newtest:
+        # Generate a new test
+        clients, orders = generateTest(
+            args.allrandom, args.clients, args.orders, args.tickers
+        )
+        # else get an existing test from a file -- TODO: implement this
     run_OrderBook(clients, orders)
