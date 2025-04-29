80233de253a6e351c90f5325f5c2924a0cd70b9a
Kirsten-E <kikielse05@outlook.com>
2025-04-29 10:38:37 +0100

    Added transaction_price parameter to create_transaction, and remove StockMarket table for simplicity

--- database_testing.py
+++ database_testing.py
@@ -12,54 +12,61 @@ from database import Database
 
 class TestDatabase(unittest.TestCase):
 
-
     # create_transaction tests
 
     # Add a transaction where bidder has no previous stock and asker has more stock than stock sold
     # Path: UPDATE, INSERT, UPDATEx2, INSERT
     def test_add_transaction_valid_1(self):
-        result = Database().create_transaction(2, 2, 1, 1, 6, 1)
+        result = Database().create_transaction(2, 2, 1, 1, 6, "A", 1)
         self.assertNotEqual(result, -1)
-        connection = sqlite3.connect('stock_market_database.db')
+        connection = sqlite3.connect("stock_market_database.db")
         cursor = connection.cursor()
         # 1 record for bidder
-        cursor.execute('''SELECT * FROM OwnedStock WHERE owner_id = 2''')
-        self.assertEqual(cursor.fetchall(), [(2, 1, 1, 6)])
+        cursor.execute("""SELECT * FROM OwnedStock WHERE owner_id = 2""")
+        self.assertEqual(cursor.fetchall(), [(2, "A", 1, 6)])
         # 1 record for asker
-        cursor.execute('''SELECT * FROM OwnedStock WHERE owner_id = 1''')
+        cursor.execute("""SELECT * FROM OwnedStock WHERE owner_id = 1""")
         self.assertEqual(len(cursor.fetchall()), 1)
-        # 1 transaction added 
-        cursor.execute('''SELECT bidder_id, bid_price, asker_id, ask_price, vol, stock_id FROM Transactions WHERE transaction_id = ?''', (result, ))
-        self.assertEqual(cursor.fetchall(), [(2, 2, 1, 1, 6, 1)])
+        # 1 transaction added
+        cursor.execute(
+            """SELECT bidder_id, bid_price, asker_id, ask_price, vol, ticker, transaction_price FROM Transactions WHERE transaction_id = ?""",
+            (result,),
+        )
+        self.assertEqual(cursor.fetchall(), [(2, 2, 1, 1, 6, "A", 1)])
         # Reset database
-        cursor.execute('''DELETE FROM OwnedStock WHERE owner_id = 2''')
+        cursor.execute("""DELETE FROM OwnedStock WHERE owner_id = 2""")
         connection.commit()
         connection.close()
-    
+
     # Add a transaction where bidder has previous stock and asker has more stock than stock sold
     # Path: UPDATE, UPDATE, UPDATEx2, INSERT
     def test_add_transaction_valid_2(self):
         # Initilise database
-        connection = sqlite3.connect('stock_market_database.db')
+        connection = sqlite3.connect("stock_market_database.db")
         cursor = connection.cursor()
-        cursor.execute('''INSERT INTO OwnedStock(owner_id, stock_id, average_price, total_vol) VALUES(2, 1, 2, 10)''')
+        cursor.execute(
+            """INSERT INTO OwnedStock(owner_id, ticker, average_price, total_vol) VALUES(2, "A", 2, 10)"""
+        )
         connection.commit()
         connection.close()
-        result = Database().create_transaction(2, 2, 1, 1, 5, 1)
+        result = Database().create_transaction(2, 2, 1, 1, 5, "A", 1)
         self.assertNotEqual(result, -1)
-        connection = sqlite3.connect('stock_market_database.db')
+        connection = sqlite3.connect("stock_market_database.db")
         cursor = connection.cursor()
         # 1 record for bidder
-        cursor.execute('''SELECT * FROM OwnedStock WHERE owner_id = 2''')
-        self.assertEqual(cursor.fetchall(), [(2, 1, 1.67, 15)])
+        cursor.execute("""SELECT * FROM OwnedStock WHERE owner_id = 2""")
+        self.assertEqual(cursor.fetchall(), [(2, "A", 1.67, 15)])
         # 1 record for asker
-        cursor.execute('''SELECT * FROM OwnedStock WHERE owner_id = 1''')
+        cursor.execute("""SELECT * FROM OwnedStock WHERE owner_id = 1""")
         self.assertEqual(len(cursor.fetchall()), 1)
-        # 1 transaction added 
-        cursor.execute('''SELECT bidder_id, bid_price, asker_id, ask_price, vol, stock_id FROM Transactions WHERE transaction_id = ?''', (result, ))
-        self.assertEqual(cursor.fetchall(), [(2, 2, 1, 1, 5, 1)])
+        # 1 transaction added
+        cursor.execute(
+            """SELECT bidder_id, bid_price, asker_id, ask_price, vol, ticker, transaction_price FROM Transactions WHERE transaction_id = ?""",
+            (result,),
+        )
+        self.assertEqual(cursor.fetchall(), [(2, 2, 1, 1, 5, "A", 1)])
         # Reset database
-        cursor.execute('''DELETE FROM OwnedStock WHERE owner_id = 2''')
+        cursor.execute("""DELETE FROM OwnedStock WHERE owner_id = 2""")
         connection.commit()
         connection.close()
 
@@ -67,88 +74,102 @@ class TestDatabase(unittest.TestCase):
     # Path: DELETE, INSERT, UPDATEx2, INSERT
     def test_add_transaction_valid_3(self):
         # Initilise database
-        connection = sqlite3.connect('stock_market_database.db')
+        connection = sqlite3.connect("stock_market_database.db")
         cursor = connection.cursor()
-        cursor.execute('''INSERT INTO OwnedStock(owner_id, stock_id, average_price, total_vol) VALUES(1, 2, 2, 5)''')
+        cursor.execute(
+            """INSERT INTO OwnedStock(owner_id, ticker, average_price, total_vol) VALUES(1, "B", 2, 5)"""
+        )
         connection.commit()
         connection.close()
-        result = Database().create_transaction(2, 2, 1, 1, 5, 2)
+        result = Database().create_transaction(2, 2, 1, 1, 5, "B", 1)
         self.assertNotEqual(result, -1)
-        connection = sqlite3.connect('stock_market_database.db')
+        connection = sqlite3.connect("stock_market_database.db")
         cursor = connection.cursor()
         # 1 record for bidder
-        cursor.execute('''SELECT * FROM OwnedStock WHERE owner_id = 2''')
-        self.assertEqual(cursor.fetchall(), [(2, 2, 1, 5)])
+        cursor.execute("""SELECT * FROM OwnedStock WHERE owner_id = 2""")
+        self.assertEqual(cursor.fetchall(), [(2, "B", 1, 5)])
         # No records for asker
-        cursor.execute('''SELECT * FROM OwnedStock WHERE owner_id = 1 AND stock_id = 2''')
+        cursor.execute(
+            '''SELECT * FROM OwnedStock WHERE owner_id = 1 AND ticker = "B"'''
+        )
         self.assertEqual(len(cursor.fetchall()), 0)
-        # 1 transaction added 
-        cursor.execute('''SELECT bidder_id, bid_price, asker_id, ask_price, vol, stock_id FROM Transactions WHERE transaction_id = ?''', (result, ))
-        self.assertEqual(cursor.fetchall(), [(2, 2, 1, 1, 5, 2)])
+        # 1 transaction added
+        cursor.execute(
+            """SELECT bidder_id, bid_price, asker_id, ask_price, vol, ticker, transaction_price FROM Transactions WHERE transaction_id = ?""",
+            (result,),
+        )
+        self.assertEqual(cursor.fetchall(), [(2, 2, 1, 1, 5, "B", 1)])
         # Reset database
-        cursor.execute('''DELETE FROM OwnedStock WHERE stock_id = 2''')
+        cursor.execute('''DELETE FROM OwnedStock WHERE ticker = "B"''')
         connection.commit()
         connection.close()
-    
+
     # Add a transaction where bidder has previous stock and asker has exactly the amount of stock
     # Path: DELETE, UPDATE, UPDATEx2, INSERT
     def test_add_transaction_valid_4(self):
         # Initilise database
-        connection = sqlite3.connect('stock_market_database.db')
+        connection = sqlite3.connect("stock_market_database.db")
         cursor = connection.cursor()
-        cursor.execute('''INSERT INTO OwnedStock(owner_id, stock_id, average_price, total_vol) VALUES(1, 2, 2, 5)''')
-        cursor.execute('''INSERT INTO OwnedStock(owner_id, stock_id, average_price, total_vol) VALUES(2, 2, 2, 10)''')
+        cursor.execute(
+            """INSERT INTO OwnedStock(owner_id, ticker, average_price, total_vol) VALUES(1, "B", 2, 5)"""
+        )
+        cursor.execute(
+            """INSERT INTO OwnedStock(owner_id, ticker, average_price, total_vol) VALUES(2, "B", 2, 10)"""
+        )
         connection.commit()
         connection.close()
-        result = Database().create_transaction(2, 2, 1, 1, 5, 2)
+        result = Database().create_transaction(2, 2, 1, 1, 5, "B", 1)
         self.assertNotEqual(result, -1)
-        connection = sqlite3.connect('stock_market_database.db')
+        connection = sqlite3.connect("stock_market_database.db")
         cursor = connection.cursor()
         # 1 record for bidder
-        cursor.execute('''SELECT * FROM OwnedStock WHERE owner_id = 2''')
-        self.assertEqual(cursor.fetchall(), [(2, 2, 1.67, 15)])
+        cursor.execute("""SELECT * FROM OwnedStock WHERE owner_id = 2""")
+        self.assertEqual(cursor.fetchall(), [(2, "B", 1.67, 15)])
         # No records for asker
-        cursor.execute('''SELECT * FROM OwnedStock WHERE owner_id = 1 AND stock_id = 2''')
+        cursor.execute(
+            '''SELECT * FROM OwnedStock WHERE owner_id = 1 AND ticker = "B"'''
+        )
         self.assertEqual(len(cursor.fetchall()), 0)
-        # 1 transaction added 
-        cursor.execute('''SELECT bidder_id, bid_price, asker_id, ask_price, vol, stock_id FROM Transactions WHERE transaction_id = ?''', (result, ))
-        self.assertEqual(cursor.fetchall(), [(2, 2, 1, 1, 5, 2)])
+        # 1 transaction added
+        cursor.execute(
+            """SELECT bidder_id, bid_price, asker_id, ask_price, vol, ticker, transaction_price FROM Transactions WHERE transaction_id = ?""",
+            (result,),
+        )
+        self.assertEqual(cursor.fetchall(), [(2, 2, 1, 1, 5, "B", 1)])
         # Reset database
-        cursor.execute('''DELETE FROM OwnedStock WHERE stock_id = 2''')
+        cursor.execute('''DELETE FROM OwnedStock WHERE ticker = "B"''')
         connection.commit()
         connection.close()
-
 
     # retrieve_specific_volumn tests
 
     # Return the specific stock owned by a user when user owns stock in that exchange
     def test_retrieve_specific_stock_valid_1(self):
-        result = Database().retrieve_specific_volumn(3, 3)
+        result = Database().retrieve_specific_volumn(3, "C")
         self.assertEqual(result, 10)
 
     # Return the specific stock owned by a user when user owns no stock in that exchange
     def test_retrieve_specific_stock_valid_2(self):
-        result = Database().retrieve_specific_volumn(3, 2)
+        result = Database().retrieve_specific_volumn(1, "C")
         self.assertEqual(result, 0)
 
     # Return the specific stock owned by a user when user does not exist
     def test_retrieve_specific_stock_edge_1(self):
-        result = Database().retrieve_specific_volumn(10000000, 3)
+        result = Database().retrieve_specific_volumn(10000000, "C")
         self.assertEqual(result, 0)
 
     # Return the specific stock owned by a user when specific stock does not exist
     def test_retrieve_specific_stock_edge_2(self):
-        result = Database().retrieve_specific_volumn(3, 100000000000)
+        result = Database().retrieve_specific_volumn(3, "Z")
         self.assertEqual(result, 0)
 
-
     # retrieve_balance tests
-    
+
     # Return the balance of a user
     def test_retrieve_balance_valid(self):
         result = Database().retrieve_balance(3)
-        self.assertEqual(result, 0)
-    
+        self.assertEqual(result, 100.0)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
